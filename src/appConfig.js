61fbdb8e541ffc0adcae6f4d0438ac81ce158d2e
endzi00 <jasarovic.enis@gmail.com>
2018-07-18 21:06:43 +0200

    added appconf

---
+++ src/appConfig.js
@@ -0,0 +1,10 @@
+export default {
+    pageTransition: false, //when true start page fade out animation 
+    transitionDuration: 500, //miliseconds,
+    googleApiKey: "AIzaSyAgxmP55sDFaAZ799wUP7i7GOId9ZzXDy4",
+    contactInfo: {
+        email: "pinomont@areon.me",
+        phone: "+382(68)070-133",
+        address: "ul. Miljana Tomičića bb. Berane - Crna Gora"
+    }
+}
